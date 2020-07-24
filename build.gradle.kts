45589ccc13ccbaf36be18e9976dddc57ff84a17e
SettingDust <settingdust@gmail.com>
2020-07-24 11:03:22 +0800

    :construction: Add build workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,42 @@
 plugins {
     kotlin("jvm") version "1.4-M3"
+    `maven-publish`
 }
 
 group = "me.settingdust"
 version = "1.0-SNAPSHOT"
+
+fun getBuildNumber(): String {
+    if (System.getenv("BUILD_NUMBER") != null) {
+        return System.getenv("BUILD_NUMBER").toString()
+    }
+    return ""
+}
+
+fun getStable(): String {
+    if (System.getenv("GITHUB_REF") == null || System.getenv("GITHUB_REF").endsWith("-dev")) {
+        return "unstable"
+    }
+    return ""
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/SettingDust/laven")
+            credentials {
+                username = project.findProperty("gpr.user") as? String ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as? String ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
 
 repositories {
     maven("https://dl.bintray.com/kotlin/kotlin-eap")
