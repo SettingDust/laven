cdd0e402a9a2345fc3ae62319bcec0ae7a0ec08b
SettingDust <settingdust@gmail.com>
2020-09-01 16:21:33 +0800

    :art: Format code

--- src/me/settingdust/laven/ReactiveFile.kt
+++ src/me/settingdust/laven/ReactiveFile.kt
@@ -7,9 +7,7 @@ import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
 import java.nio.file.FileSystems
 import java.nio.file.Path
-import java.nio.file.StandardWatchEventKinds.ENTRY_CREATE
-import java.nio.file.StandardWatchEventKinds.ENTRY_DELETE
-import java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY
+import java.nio.file.StandardWatchEventKinds.*
 import java.nio.file.WatchEvent
 
 @ExperimentalCoroutinesApi
